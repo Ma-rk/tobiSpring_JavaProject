ba86782dd606b789bcbfcf486c27f6f21f94be95
Mark K <k.mark.kr@gmail.com>
2015-06-13 22:34:42 +0900

    step 07 - created class: DaoFactory(2)

--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,9 +1,14 @@
 package dao;
 
+import core.ConnectionMaker;
 import core.DConnectionMaker;
 
 public class DaoFactory {
 	public UserDao userDao() {
-		return new UserDao(new DConnectionMaker());
+		return new UserDao(connectionMaker());
+	}
+
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }

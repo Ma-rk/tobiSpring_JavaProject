de6b02ede65bc6a77c34adf4d387438e716d2ba0
Mark K <k.mark.kr@gmail.com>
2015-06-14 02:13:47 +0900

    step 08 - AnnotationConfigApplicationContext

--- src/core/UserDaoTest.java
+++ src/core/UserDaoTest.java
@@ -2,13 +2,17 @@ package core;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import dao.DaoFactory;
 import dao.UserDao;
 import entity.UserEntity;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 
 		UserEntity user = new UserEntity();
 		user.setId("idid");
--- src/dao/DaoFactory.java
+++ src/dao/DaoFactory.java
@@ -1,13 +1,19 @@
 package dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import core.ConnectionMaker;
 import core.DConnectionMaker;
 
+@Configuration
 public class DaoFactory {
+	@Bean
 	public UserDao userDao() {
 		return new UserDao(connectionMaker());
 	}
 
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}

c4a59b5d9b9fa169c10c20ab388d486bfed2302d
Mark K <k.mark.kr@gmail.com>
2015-06-14 20:18:30 +0900

    step 16 - spring test context framework

--- test/dao/UserDaoTest.java
+++ test/dao/UserDaoTest.java
@@ -4,55 +4,64 @@ import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import dao.UserDao;
 import entity.UserEntity;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
+	@Autowired
+	private ApplicationContext context;
+
+	private UserDao dao;
+	private UserEntity user1;
+	private UserEntity user2;
+
+	@Before
+	public void setup() {
+		this.dao = context.getBean("userDao", UserDao.class);
+
+		this.user1 = new UserEntity("idid", "namename", "pwpw");
+		this.user2 = new UserEntity("idid2", "nmnm", "pwpw");
+	}
 
 	@Test
 	public void addUserTest() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
+		dao.add(user1);
 
-		UserEntity user = new UserEntity();
-		user.setId("idid");
-		user.setName("namename");
-		user.setPassword("pwpw");
+		UserEntity retrievedUser = dao.get(user1.getId());
 
-		dao.add(user);
-
-		UserEntity user2 = dao.get(user.getId());
-
-		assertEquals(user.getId(), user2.getId());
-		assertEquals(user.getName(), user2.getName());
-		assertEquals(user.getPassword(), user2.getPassword());
+		assertEquals(user1.getId(), retrievedUser.getId());
+		assertEquals(user1.getName(), retrievedUser.getName());
+		assertEquals(user1.getPassword(), retrievedUser.getPassword());
 	}
 
 	@Test
 	public void addAndGet() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
-
 		dao.deleteAll();
 		assertEquals(0, dao.getCout());
 
-		dao.add(new UserEntity("idid", "nmnm", "pwpw"));
+		dao.add(user1);
 		assertEquals(1, dao.getCout());
 
-		dao.add(new UserEntity("idid2", "nmnm", "pwpw"));
+		dao.add(user2);
 		assertEquals(2, dao.getCout());
+
+		dao.deleteAll();
+		assertEquals(0, dao.getCout());
 	}
 
 	@Test(expected = EmptyResultDataAccessException.class)
 	public void getUserFailure() throws SQLException, ClassNotFoundException {
-		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-		UserDao dao = context.getBean("userDao", UserDao.class);
-
 		dao.deleteAll();
 		assertEquals(0, dao.getCout());
 
